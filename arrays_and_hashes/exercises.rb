89c5ac3830437b20e205e091e279409eceedfb75
naderae <naderabouezze93@gmail.com>
2017-06-28 14:24:17 -0400

    exercise1

---
+++ arrays_and_hashes/exercises.rb
@@ -0,0 +1,38 @@
+colors= ['red', 'blue', 'green']
+ages = ['25', '20', '30']
+coin_flip =['heads', 'heads', 'tails']
+artists = ['biggie', 'tupac', 'nas']
+colors = [:red, :blue, :green]
+
+
+definitions = {
+  'demented' => 'crazy',
+  'inquisitive' => 'curious',
+  'jovial' => 'happy',
+}
+
+movies = {
+  scarface: '1983',
+  godfather: '1972',
+  casino: '1995',
+}
+
+cities = {
+  :beirut => 1000000,
+  :toronto => 6500000,
+  :rome => 3000000,
+}
+
+
+sibling_ages = {
+  "marwan" => 24,
+  "waseem" => 29,
+  "rheam" => 32
+}
+
+puts coin_flip
+puts colors[0]
+puts ages.sort
+ages << '0'
+puts ages
+puts movies[:scarface]
